de0f4da8b92e4396da8dd798a41a1d037d466a1f
Logan Thomas <ltogan.2014@gmail.com>
2024-02-24 16:06:01 -0500

    Update unit tests

--- test/vertex_test.cpp
+++ test/vertex_test.cpp
@@ -1,151 +1,90 @@
+#include <functional>
 #include <gtest/gtest.h>
-#include <graph_lib/graph/vertex.hpp>
+#include <graph_lib/vertex.hpp>
 
-namespace wfc = graph_lib;
 
-class VertexTestBasic : public testing::Test
+class VertexTestCustomClass : public testing::Test
 {
 public:
-    wfc::Vertex<int> v1{1};
-    wfc::Vertex<int> v2{2};
+  struct MyTestStruct
+  {
+    unsigned int x;
+
+    MyTestStruct(int val): x(val) {}
+
+    // Custom data type must implement operator==
+    inline bool        operator==(const MyTestStruct& rhs)                          const { return x == rhs.x; }
+    inline bool        operator==(const unsigned int& rhs)                          const { return x == rhs; }
+    inline friend bool operator==(const unsigned int& lhs, const MyTestStruct& rhs)       { return lhs == rhs.x; }
+
+    // TODO: Remove this after testing
+    inline friend std::ostream& operator<<(std::ostream& os, const MyTestStruct& test) { os << test.x; return os;}
+  };
 };
 
-TEST(VertexTest, OperatorIsEqual)
+// Custom data type must be hashable
+template <>
+struct std::hash<VertexTestCustomClass::MyTestStruct>
 {
-    wfc::Vertex<int> v1{1};
-    wfc::Vertex<int> v2{1};
-    EXPECT_TRUE(v1 == v2);
-    EXPECT_TRUE(v1 == 1);
-    EXPECT_TRUE(1 == v1);
+  size_t operator()(const VertexTestCustomClass::MyTestStruct &val) const
+  {
+    return std::hash<unsigned int>()(val.x);
+  }
+};
+
+
+TEST(VertexTest, TestIDIncrement)
+{
+  graph_lib::Vertex<int> v1{2};
+  EXPECT_EQ(v1.getID(), 0);
+
+  graph_lib::Vertex<int> v2{2};
+  EXPECT_EQ(v2.getID(), 1);
+
+  // Ensure v1 id unchanged
+  EXPECT_EQ(v1.getID(), 0);
 }
 
-TEST_F(VertexTestBasic, OperatorNotIsEqual)
+TEST(VertexTest, TestData)
 {
-    EXPECT_FALSE(v1 == v2);
-    EXPECT_FALSE(v1 == 2);
-    EXPECT_FALSE(1 == v2);
+  graph_lib::Vertex<int> v1{1};
+  EXPECT_EQ(*(v1.data), 1);
+
+  graph_lib::Vertex<std::string> v2{"Test"};
+  EXPECT_EQ(*(v2.data), "Test");
+
+  graph_lib::Vertex<float> v3{1.2345f};
+  EXPECT_FLOAT_EQ(*(v3.data), 1.2345f);
+
+  graph_lib::Vertex<double> v4{6.7890};
+  EXPECT_DOUBLE_EQ(*(v4.data), 6.7890);
 }
 
-TEST_F(VertexTestBasic, OperatorIsNotEqual)
+TEST_F(VertexTestCustomClass, TestCustomData)
 {
-    EXPECT_TRUE(v1 != v2);
-    EXPECT_TRUE(v1 != 2);
-    EXPECT_TRUE(2 != v1);
+  graph_lib::Vertex<MyTestStruct> v1{MyTestStruct(1)};
+  EXPECT_EQ(v1, MyTestStruct(1));
+  EXPECT_EQ(v1, 1);
 }
 
-TEST(VertexTest, OperatorNotIsNotEqual)
+TEST(VertexTest, TestOperatorIsEqual)
 {
-    wfc::Vertex<int> v1{1};
-    wfc::Vertex<int> v2{1};
-    EXPECT_FALSE(v1 != v2);
-    EXPECT_FALSE(v1 != 1);
-    EXPECT_FALSE(1 != v2);
+  graph_lib::Vertex<int> v1{1};
+  graph_lib::Vertex<int> v2{1};
+
+  EXPECT_TRUE(v1 == v2);
+  EXPECT_TRUE(v1 == 1);
+  EXPECT_TRUE(1 == v1);
 }
 
-TEST_F(VertexTestBasic, OperatorGT)
+TEST(VertexTest, TestOperatorNotIsEqual)
 {
-    EXPECT_FALSE(v1 > v1);
-    EXPECT_FALSE(v1 > 1);
-    EXPECT_FALSE(1 > v1);
+  graph_lib::Vertex<int> v1{1};
+  graph_lib::Vertex<int> v2{2};
 
-    EXPECT_FALSE(v2 > v2);
-    EXPECT_FALSE(v2 > 2);
-    EXPECT_FALSE(2 > v2);
-
-    EXPECT_FALSE(v1 > v2);
-    EXPECT_FALSE(v1 > 2);
-    EXPECT_FALSE(1 > v2);
-
-    EXPECT_TRUE(v2 > v1);
-    EXPECT_TRUE(v2 > 1);
-    EXPECT_TRUE(2 > v1);
-}
-
-TEST_F(VertexTestBasic, OperatorGTE)
-{
-    EXPECT_TRUE(v1 >= v1);
-    EXPECT_TRUE(v1 >= 1);
-    EXPECT_TRUE(1 >= v1);
-
-    EXPECT_TRUE(v2 >= v2);
-    EXPECT_TRUE(v2 >= 2);
-    EXPECT_TRUE(2 >= v2);
-
-    EXPECT_FALSE(v1 >= v2);
-    EXPECT_FALSE(v1 >= 2);
-    EXPECT_FALSE(1 >= v2);
-
-    EXPECT_TRUE(v2 >= v1);
-    EXPECT_TRUE(v2 >= 1);
-    EXPECT_TRUE(2 >= v1);
-}
-
-TEST_F(VertexTestBasic, OperatorLT)
-{
-    EXPECT_FALSE(v1 < v1);
-    EXPECT_FALSE(v1 < 1);
-    EXPECT_FALSE(1 < v1);
-
-    EXPECT_FALSE(v2 < v2);
-    EXPECT_FALSE(v2 < 2);
-    EXPECT_FALSE(2 < v2);
-
-    EXPECT_TRUE(v1 < v2);
-    EXPECT_TRUE(v1 < 2);
-    EXPECT_TRUE(1 < v2);
-
-    EXPECT_FALSE(v2 < v1);
-    EXPECT_FALSE(v2 < 1);
-    EXPECT_FALSE(2 < v1);
-}
-
-TEST_F(VertexTestBasic, OperatorLTE)
-{
-    EXPECT_TRUE(v1 <= v1);
-    EXPECT_TRUE(v1 <= 1);
-    EXPECT_TRUE(1 <= v1);
-
-    EXPECT_TRUE(v2 <= v2);
-    EXPECT_TRUE(v2 <= 2);
-    EXPECT_TRUE(2 <= v2);
-
-    EXPECT_TRUE(v1 <= v2);
-    EXPECT_TRUE(v1 <= 2);
-    EXPECT_TRUE(1 <= v2);
-
-    EXPECT_FALSE(v2 <= v1);
-    EXPECT_FALSE(v2 <= 1);
-    EXPECT_FALSE(2 <= v1);
-}
-
-TEST_F(VertexTestBasic, ArithmeticOperators)
-{
-    EXPECT_EQ(v1 + v2, 3);
-    EXPECT_EQ(v1 + 2, 3);
-    EXPECT_EQ(1 + v2, 3);
-
-    EXPECT_EQ(v1 - v2, -1);
-    EXPECT_EQ(v1 - 2, -1);
-    EXPECT_EQ(1 - v2, -1);
-
-    EXPECT_EQ(v1 * v2, 2);
-    EXPECT_EQ(v1 * 2, 2);
-    EXPECT_EQ(1 * v2, 2);
-
-    EXPECT_EQ(v1 / v2, 0);
-    EXPECT_EQ(v1 / 2, 0);
-    EXPECT_EQ(1 / v2, 0);
-
-    EXPECT_EQ(v1 % v2, 1);
-    EXPECT_EQ(v1 % 2, 1);
-    EXPECT_EQ(1 % v2, 1);
-}
-
-TEST_F(VertexTestBasic, BitwiseOperators)
-{
-    EXPECT_EQ(v1 ^ v2, 3);
-    EXPECT_EQ(v1 ^ 2, 3);
-    EXPECT_EQ(1 ^ v2, 3);
+  EXPECT_FALSE(v1 == v2);
+  EXPECT_FALSE(v1 == 2);
+  EXPECT_FALSE(1 == v2);
 }
 
 int main(int argc, char **argv) {
